cf2ee7bacf68cb35060dd32c11c4c6f60979f636
epoxnet <shamimdehghani@Shamims-MacBook-Pro.local>
2019-02-23 11:54:04 -0500

    added iOS project

---
+++ FoodFinderApp/FoodFinderApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodFinderApp
+//
+//  Created by Shamim Dehghani on 2/23/19.
+//  Copyright © 2019 Sean Dehghani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
