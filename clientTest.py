94012bcdb14e8b3c16cf2a1fa38f4f9fbff914b5
JLap <jlap4567@gmail.com>
2019-02-23 16:05:50 -0500

    editted server code

--- clientTest.py
+++ clientTest.py
@@ -5,8 +5,8 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('172.30.136.211', 10000)
-print('connecting to {} port {}'.format(*server_address))
+server_address = ('http://e7018a4a.ngrok.io', 10000)
+#print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
 try:
